b1cc6a816222c525d792558330cd13a69c95e522
NadaTn <120678246+NadaTn@users.noreply.github.com>
2022-12-15 16:45:09 -0300

    commit Inicial

---
+++ scrips.js
@@ -0,0 +1,8 @@
+
+obtenerNombre= prompt ("Hola soy Amaia nesecito tu nombre para seguir")
+let nombre = obtenerNombre;
+
+let htmlCode = ` <h2>${nombre}</h2>
+<p>Te quiero invitar a festejar mi cumple años Nro 5 el dia <b>28/12/22</b> a partir de las  <b>19hs</b> en el club <b>Laureles</b> ubicado por calle 25 de agosto.
+      espero tu confirmacion por WhatsApp Gracias   </p>`
+document.querySelector(".capa").innerHTML+=htmlCode 
